84a723861969fb11c2cfe1eff28ff7970accf0e0
Maria Lupiak <maria.lupiak@gmail.com>
2025-04-11 01:07:55 +0300

    add models

---
+++ Server/ShelterService/ShelterService/Models/Entities/Animal.cs
@@ -0,0 +1,14 @@
+﻿namespace ShelterService.Models.Entities
+{
+    public class Animal
+    {
+        public int ShelterId { get; set; }
+        public virtual Shelter? Shelter { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public string? Species { get; set; }
+        public string? Age { get; set; }
+        public string? Health { get; set; }
+        public string? PhotoUrl { get; set; }
+    }
+}
---
+++ Server/ShelterService/ShelterService/Models/ShelterCategory.cs
@@ -0,0 +1,9 @@
+﻿namespace ShelterService.Models
+{
+    public enum ShelterCategory
+    {
+        VetClinic = 1,
+        DogCatShelter = 2,
+        Kennel = 3 //розплідник
+    }
+}
