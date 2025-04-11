86283354a513398d7ee5d29f0c2f9ca258dcdf2d
Denys Kuksin <deniskuksin1@gmail.com>
2025-04-12 01:07:06 +0300

    Add filter. Add temp navigation

--- TestTask/client/src/app/pages/animals/animal-list/animal-list.component.ts
+++ TestTask/client/src/app/pages/animals/animal-list/animal-list.component.ts
@@ -10,6 +10,14 @@ import { AnimalsService } from '../../../services/animals.service';
 })
 export class AnimalListComponent {
   animals: Animal[] = [];
+  filteredAnimals: Animal[] = [];
+
+  filter = {
+    species: '',
+    age: '',
+    health: ''
+  };
+
 
   constructor(private animalsService : AnimalsService) {
 
@@ -24,8 +32,17 @@ export class AnimalListComponent {
         next: response => {
           if (response) {
             this.animals = response;
+            this.filteredAnimals = response;
           }
         }
       });
+  }
+
+  applyFilter() {
+    this.filteredAnimals = this.animals.filter(animal =>
+      (!this.filter.species || animal.species?.toLowerCase().includes(this.filter.species.toLowerCase())) &&
+      (!this.filter.age || animal.age?.toLowerCase().includes(this.filter.age.toLowerCase())) &&
+      (!this.filter.health || animal.health?.toLowerCase().includes(this.filter.health.toLowerCase()))
+    );
   }
 }
